02dd0a838e40a885ce8f4192b352f9ec91a85b91
Mina Morena <minamorena93@hotmail.com>
2025-08-27 11:33:26 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.devsuperior.hrpayroll.services;
+
+import com.devsuperior.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        //por enquanto vamos deixar valores mockados aqui
+        return new Payment("Bob", 200.0, days);
+    }
+}
